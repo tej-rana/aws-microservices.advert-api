b7d5148c6d6be6626f481b5d67e62b8607e372d0
Tej Rana <tej@reosoftware.com.au>
2020-12-27 04:16:01 +1000

    Updated Nuget package for AdvertModels

--- AdvertApi/Services/DynamoDbAdvertStorage.cs
+++ AdvertApi/Services/DynamoDbAdvertStorage.cs
@@ -51,6 +51,7 @@ namespace AdvertApi.Services
 
                     if (model.Status == AdvertStatus.Active)
                     {
+                        record.FilePath = model.FilePath;
                         record.Status = AdvertStatus.Active;
                         await context.SaveAsync(record);
                     }
